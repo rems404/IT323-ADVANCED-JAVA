a86998dc29dbfa076c6dfb122daa0da3e8f2f9a4
rems404 <164834503+rems404@users.noreply.github.com>
2025-03-09 18:17:34 +0800

    Update WT1_guidedTasks.java

--- Walk-through 1/WT1_guidedTasks.java
+++ Walk-through 1/WT1_guidedTasks.java
@@ -1,23 +1,28 @@
 import java.time.LocalTime;
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class WT1_guidedTasks {
     
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
         // guided task #1
         String myName = "Remy Dianne Ventura";
         System.out.println("GUIDED TASK #1 \nWelcome, " + myName + "!");
         
         // guided task #2
-        double length = 10.33;
-        double width = 5.7;
+        System.out.print("\nGUIDED TASK #2 (area of a rectangle) \nEnter length: ");
+        double length = scan.nextDouble();
+        System.out.print("Enter width: ");
+        double width = scan.nextDouble();
         double area = length * width;
-        System.out.println("\nGUIDED TASK #2 \nLength of the rectangle: " + length + "\nWidth of the rectangle: " + width + "\nArea of the rectangle: " + area);
+        System.out.println("Area: " + area);
         
         // guided task #3
         LocalDate todayIs = LocalDate.now();
         LocalTime timeIs = LocalTime.now();
-        System.out.println("\nGUIDED TASK #3 \nToday is: " + todayIs + "\nTime is: " + timeIs);
+        System.out.println("\nGUIDED TASK #3 \nDate: " + todayIs + "\nTime: " + timeIs);
     }
     
 }
